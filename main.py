1cf5c36fe208a25dd42c48886463e4fe637f34d3
resfer3 <resfer333@gmail.com>
2024-12-18 19:37:49 -0700

    implemented character count and terminal report print

--- main.py
+++ main.py
@@ -8,21 +8,31 @@ def count_words(txt):
   return counter
 
 def count_non_duplicate_chars(txt):
-  # get rid of duplicates
-  # TODO
+  char_dict = {}
+  storage = ""
   for char in txt:
-  txt_set = set(txt)
-  txt_set = str(txt_set).lower()
-  new_dict = {}
-  for char in txt_set:
-    new_dict[char] = 0
-  return new_dict
+    if char.isalpha():
+      storage = char.lower()
+    else:
+      continue
+    if storage in char_dict:
+      char_dict[storage] += 1
+    else:
+      char_dict[storage] = 1
+  return char_dict
+
 
 def main():
   with open("books/frankenstein.txt") as f:  
     file_contents = f.read()
-    #print(count_words(file_contents))
-    print(count_non_duplicate_chars(file_contents))
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words(file_contents)} words found in the document")
+    print()
+
+    char_dict = count_non_duplicate_chars(file_contents)
+    for key in char_dict:
+      print(f"The '{key}' character was found {char_dict[key]} times")
+    print("--- End report ---")
 
 
 main()
