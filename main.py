bd3f68346e3c6f42a3acc0563b365f7d4d2625b5
resfer3 <resfer333@gmail.com>
2025-05-03 20:20:46 -0600

    created function to get a sorted list of dictionaries of chars

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ def main():
   file = "books/frankenstein.txt"
   # print(get_book_text("books/frankenstein.txt"))
   count = count_words(file)
-  print(f"{count} words found in the document")
   letter_count = count_chars(file)
-  print(f"{letter_count}")
+  # print(f"{letter_count}")
+
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {file}...")
+  print("----------- Word Count ----------")
+  print(f"Found {count} total words")
+  print("--------- Character Count -------")
+
+  sorted_list = sorted_chars(letter_count)
+  for dict in sorted_list:
+    if dict["char"].isalpha():
+      print(f"{dict["char"]}: {dict["num"]}")
+
+
+  # for k in letter_count:
+    # print(f"{k}: {letter_count[k]}")
   
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,4 @@
+# count how many words are in the text
 def count_words(book):
   book_str = ""
   with open(book) as file:
@@ -6,6 +7,7 @@ def count_words(book):
     
   return len(book_str)
 
+# count characters of book
 def count_chars(book):
   chars_dict = {}
   with open(book) as file:
@@ -21,7 +23,18 @@ def count_chars(book):
 
     return chars_dict
 
+# return a sorted list of dictionaries
+def sorted_chars(dict):
+    list_chars = []
+    for k in dict:
+        tmp_dict = {}
+        tmp_dict["char"] = k
+        tmp_dict["num"] = dict[k]
+        list_chars.append(tmp_dict)
+    return sorted(list_chars,reverse=True, key=lambda i:i["num"])
 
+
+# get full book text
 def get_book_text(book):
   with open(book) as file:
     book_contents = file.read()
